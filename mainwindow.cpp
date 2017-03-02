0abb0f95e33ccb51e530e50f4f89f3a11b0a72ed
Rocker98 <y.petrunyak@gmail.com>
2017-03-02 22:49:06 +0200

    Make design. Nothink works except exit()

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->dateTimeEdit->setDateTime(QDateTime::currentDateTimeUtc());
+
 }
 
 MainWindow::~MainWindow()
